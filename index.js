40109fbabd179cfadaa9881279b315c4fc20fee2
sahilrawat001 <sahilrawat80@gmail.com>
2021-08-24 22:49:14 -0700

    data change

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-const express= require("express");
-const database= require("./database");
 
+const express= require("express");
+const Database= require("./database");
 const ourApp=express();
+//home 
+
+ourApp.get("/", (request,response)=>{
+    response.json({message:"request is being  served  "})
+} );
+ourApp.get("/book", (request,response)=>{
+   return response.json({books:Database.Book});
+} );
+  
+
+
+//book 
+
+
+
+ourApp.listen(4000,()=> 
+    console.log("its working")
+); 
